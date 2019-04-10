0268f08da172e481502cd6c866c51864cc2d53cc
Leon Drygala <leon.drygala1@cba.com.au>
2019-04-10 18:57:10 +1000

    Implement hashing keys

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,28 @@ fn main() {
 
     let mut my_map: HashMap<String, String> = HashMap::new();
     assert_eq!(my_map.len(), 0);
-    let my_key = "my key";
-    let my_val = "Hello, world!";
-    my_map = my_map.insert(my_key.to_string(), my_val.to_string());
+    let my_key = "my key".to_string();
+    let my_val = "Hello, world!".to_string();
+    my_map = my_map.insert(my_key.clone(), my_val.clone());
     assert_eq!(my_map.len(), 1);
-    my_map = my_map.insert("my key2".to_string(), "Second value".to_string());
-    assert_eq!(my_map.len(), 2);
-    my_map = my_map.insert("ooooops".to_string(), "Third value".to_string());
-    assert_eq!(my_map.get(&"my key2".to_string()), "Third value");
+    assert_eq!(my_map.get(&my_key), Some(my_val));
+    println!("Assertions passed!");
 }
 
 
 
 mod my_hash_map {
 use std::option::Option;
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
 
-    pub struct MyHashMap<K: Clone, V: Clone> {
-        store: [Option<(K, V)>; 3],
+    pub struct MyHashMap<K: Clone + Hash, V: Clone> {
+        store: [Option<(K, V)>; 31],
     }
 
-    impl<K: Clone, V: Clone> MyHashMap<K, V> {
+    impl<K: Clone + Hash, V: Clone> MyHashMap<K, V> {
         pub fn new() -> MyHashMap<K, V> {
-            return MyHashMap { store: [None, None, None] };
+            return MyHashMap { store: Default::default() };
         }
 
         pub fn len(&self) -> usize {
@@ -44,16 +44,15 @@ use std::option::Option;
             return self.insert_at(key, val, i);
         }
 
-        pub fn get(&self, key: &K) -> V {
-            return self.store[self.hash_key(key)].clone().unwrap().1;
+        pub fn get(&self, key: &K) -> Option<V> {
+            return self.store[self.hash_key(key)].clone().map(|p| p.1);
         }
 
-        fn hash_key(&self, _key: &K) -> usize {
-            let hash: usize;
-            unsafe {
-                hash = core::arch::x86_64::_rdrand16_step(&mut 0u16) as usize;
-            }
-            return hash.checked_rem(self.store.len()).unwrap();
+        fn hash_key(&self, key: &K) -> usize {
+            let mut s = DefaultHasher::new();
+            key.hash(&mut s);
+            let hash = s.finish();
+            return hash.checked_rem(self.store.len() as u64).unwrap() as usize;
         }
 
         fn insert_at(& self, _key: K, _val: V, i: usize) -> MyHashMap<K, V> {
@@ -62,7 +61,6 @@ use std::option::Option;
             return MyHashMap { store: new_store, };
         }
     }
-
 }
 
 #[cfg(test)]
@@ -78,17 +76,29 @@ use my_hash_map::MyHashMap as HashMap;
     #[test]
     fn insert_and_get() {
         let mut my_map: HashMap<String, String> = HashMap::new();
-        let my_key = "my key";
-        let my_val = "Hello, world!";
-        my_map.insert(my_key.to_string(), my_val.to_string());
-        let act_result = my_map.get(my_key);
-        assert_eq!(Some(&my_val.to_string()), act_result);
+        let my_key = "my key".to_string();
+        let my_val = "Hello, world!".to_string();
+        my_map = my_map.insert(my_key.clone(), my_val.clone());
+        let act_result = my_map.get(&my_key);
+        assert_eq!(Some(my_val), act_result);
+    }
+
+    #[test]
+    fn insert_and_get3() {
+        let mut my_map: HashMap<String, String> = HashMap::new();
+        let my_key = "my key".to_string();
+        let my_val = "Hello, world!".to_string();
+        my_map = my_map.insert(my_key.clone(), my_val.clone());
+        my_map = my_map.insert("key2".to_string(), "val2".to_string());
+        my_map = my_map.insert("key3".to_string(), "val3".to_string());
+        let act_result = my_map.get(&my_key);
+        assert_eq!(Some(my_val), act_result);
     }
 
     #[test]
     fn get_nothin() {
         let my_map = get_new_map();
-        let act_result = my_map.get("Not a key");
+        let act_result = my_map.get(&"Not a key".to_string());
         assert_eq!(act_result, None);
     }
 
@@ -102,9 +112,9 @@ use my_hash_map::MyHashMap as HashMap;
     #[test]
     fn len_1() {
         let mut my_map = HashMap::new();
-        let my_key = "my key";
-        let my_val = "Hello, world!";
-        my_map.insert(my_key, my_val);
+        let my_key = "my key".to_string();
+        let my_val = "Hello, world!".to_string();
+        my_map = my_map.insert(my_key, my_val);
         assert_eq!(1, my_map.len());
     }
 }
